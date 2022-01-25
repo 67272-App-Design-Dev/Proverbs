c247b554b3b1a89808bcd297cf47c9f79fd39faf
Prof. H <profh@cmu.edu>
2022-01-25 09:44:10 -0500

    Additional teaching note in Home Controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,9 @@ class HomeController < ApplicationController
     # get the data you want
     @proverb = Proverb.get_random_quote
 
+    # template: 'martok'
+    # layout: 'application'
+
     # send to a view template and layout
     # done by default to home#index
   end
